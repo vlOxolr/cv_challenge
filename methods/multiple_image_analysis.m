b1cf61b9e5bc57ef4c5850f6181ade49f1fa4b8a
shuwenLLL <lsw970202@gmail.com>
2025-07-04 17:07:04 +0200

    Update multiple_image_analysis: match with previous image, align to first, fill from previous

--- methods/multiple_image_analysis.m
+++ methods/multiple_image_analysis.m
@@ -8,20 +8,18 @@ function imgs = multiple_image_analysis(original_imgs)
     ref_img = original_imgs{1};%image 1 reference
     %gtrafo = [1,0,0;0,1,0;0,0,1]; % general transformation matrix
     for i = 2:size(original_imgs,2)
-         ref_img_for_transform = original_imgs{1};     % For alignment direction (always 1st image)
-         ref_img_for_fill = imgs{i-1};                % For filling (it's the last picture after the alignment)
-         cur_img = original_imgs{i};
+        matching_ref_img = original_imgs{i-1};     
+        cur_img = original_imgs{i};
+        align_ref_img = original_imgs{1};
         %ref_img = original_imgs{i-1};%image i-1 reference
-        [proc_ref, proc_cur] = preprocessing(ref_img_for_transform, cur_img);
+        [proc_ref, proc_cur] = preprocessing(matching_ref_img, cur_img);
         [trafo,status] = SURFmatching(proc_ref, proc_cur, false);
         %gtrafo = double(trafo.A) * gtrafo;
-        
+        fill_img = imgs{i-1};
         %outputView = imref2d(size(img1));
         if status == 0
-            corrected_img = trafo_correction(cur_img, trafo);%simtform2d(gtrafo));
-            %corrected_img = align_and_fill(original_imgs{i}, ref_img, trafo);
-            % make imgAafter complete using imgB
-            corrected_img(corrected_img==0)=ref_img_for_fill(corrected_img==0);
+            corrected_img = trafo_correction(cur_img, trafo);  
+            corrected_img(corrected_img==0) = fill_img(corrected_img==0);
             imgs{end+1} = corrected_img;
         else
             warning('Image %d could not be matched and was skipped.', i);
