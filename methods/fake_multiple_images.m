a387c6cec91119be1e879441ac6e23ba62241743
Bangdong Zhang <1027199747@qq.com>
2025-07-12 06:25:53 +0200

    clean up

--- methods/fake_multiple_images.m
+++
@@ -1,8 +0,0 @@
-function imgs = multiple_images(oringial_imgs)
-% input:
-% original_imgs: cell which contains two original images, and imgs must
-% be in time order
-% output:
-% imgs: cell which contains two corrected images
-    imgs = oringial_imgs;
-end
